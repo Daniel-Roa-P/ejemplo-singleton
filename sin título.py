10d1b84e041cc759049869d7b3099e01a3082101
DanielRoa20171020077 <42393971+DanielRoa20171020077@users.noreply.github.com>
2018-08-22 12:17:23 -0500

    Update sin título.py

--- sin título.py
+++ sin título.py
@@ -11,12 +11,12 @@ class Ejemplo_singleton(object):
             Ejemplo_singleton.__instance = object.__new__(cls)
         return Ejemplo_singleton__instance
 
-ricardo = Ejemplo_singleton()
-ricardo.valor = "instanciacion 1"
-print(ricardo)
-ramon = Ejemplo_singleton()
-ramon.valor = "instanciacion 2"
-print(ramon)
+llamado_1 = Ejemplo_singleton()
+llamado_1.valor = "instanciacion 1"
+print(llamado_1)
+llamado_2= Ejemplo_singleton()
+llamado_2.valor = "instanciacion 2"
+print(llamado_2)
 
-print(ricardo)
-print(ramon)
+print(llamado_1)
+print(llamado_2)
